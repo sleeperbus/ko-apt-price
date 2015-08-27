975394c7bdbd43b98daec4f7a5a7a7a5ee364e54
Hojoon Ji <sleeperbus@gmail.com>
2015-08-27 17:56:11 +0900

    테스트 중

--- exploratory.R
+++ exploratory.R
@@ -2,11 +2,18 @@ library(dygraphs)
 ################################################################################
 # dygraph 테스트
 ################################################################################
-str(mdeaths)
 lungDeaths <- cbind(mdeaths, fdeaths)
 str(lungDeaths)
 dygraph(lungDeaths)
-
 dygraph(lungDeaths) %>% dyRangeSelector()
 
 
+################################################################################
+# 인천서구 데이터로 놀이
+################################################################################
+library(plyr)
+source("helpers.R")
+apts = f_readLocalGugunData("t", "28260", 2010, 2015)
+countByTime = count(apts, c("SALE_YEAR", "SALE_MONTH"))
+z = ts(data = countByTime$freq, start = c(2006, 1), frequency = 12)
+dygraph(z)
